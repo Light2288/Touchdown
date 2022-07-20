ca2c15e713d4d44fc42327e1eb140e83a4215303
Light2288 <davide.aliti@gmail.com>
2022-07-21 00:59:15 +0200

    Initial Commit

---
+++ Touchdown/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by Davide Aliti on 21/07/22.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
